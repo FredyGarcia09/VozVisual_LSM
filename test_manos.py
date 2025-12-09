54b5191c486b035433318d4dd38c2b23941b1a23
FredyGarcia09 <s23120180@alumnos.itsur.edu.mx>
2025-12-09 15:51:35 -0600

    Fix: Agregado contador de FPS que faltaba

--- test_manos.py
+++ test_manos.py
@@ -1,18 +1,23 @@
 import cv2
 import mediapipe as mp
+import time
 
-# 1. Configuración de MediaPipe (El "Cerebro" que ve manos)
+# Configuración de MediaPipe
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-# 2. Inicializar la webcam (0 suele ser la cámara default)
+# Inicializar webcam
 cap = cv2.VideoCapture(0)
 
-# 3. Configurar el modelo de detección
+# Variables para calcular FPS
+tiempo_anterior = 0
+tiempo_actual = 0
+
+# Configurar modelo
 with mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=2,             # Queremos detectar hasta 2 manos
-    min_detection_confidence=0.5, # Qué tan seguro debe estar para decir "es una mano"
+    max_num_hands=2,
+    min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
 
     print("Iniciando cámara... presiona 'Esc' para salir.")
@@ -20,33 +25,40 @@ with mp_hands.Hands(
     while cap.isOpened():
         success, image = cap.read()
         if not success:
-            print("No se pudo obtener el video de la cámara.")
+            print("No se pudo obtener el video.")
             continue
 
-        # 4. Preparar la imagen
-        # MediaPipe usa color RGB, pero OpenCV usa BGR. Hay que convertirlo.
+        # Preparar imagen
         image.flags.writeable = False
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
-        # 5. ¡Hacer la predicción! (Aquí ocurre la magia de IA)
+        # Predicción
         results = hands.process(image_rgb)
 
-        # 6. Dibujar los resultados
+        # Dibujar resultados
         image.flags.writeable = True
         image = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
         
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                # Dibujamos los puntos (nudos) y las conexiones (huesos)
                 mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS)
 
-        # 7. Mostrar en pantalla
+        # FPS
+        tiempo_actual = time.time()
+        # segundo / la diferencia de tiempo entre cuadros
+        fps = 1 / (tiempo_actual - tiempo_anterior)
+        tiempo_anterior = tiempo_actual
+
+        # Mostrar FPS en pantalla
+        cv2.putText(image, f'FPS: {int(fps)}', (10, 70), 
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+        # Mostrar
         cv2.imshow('VozVisual LSM - Prueba de Camara', image)
 
-        # Salir si presionas la tecla ESC
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
